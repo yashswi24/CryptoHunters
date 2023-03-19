50efd8c2d92762b1f0d9534c7fbd0464111bc245
yashswi24 <yashswi2412@gmail.com>
2023-03-20 00:48:23 +0530

    Initial commit

---
+++ src/pages/Analysis.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Analysis = () => {
+  return (
+    <div
+    className='mt-10'
+    >
+      
+      <iframe src="https://cryptohunters.streamlit.app/" width="300" height="300px" ></iframe>
+    </div>
+  )
+}
+
+export default Analysis
