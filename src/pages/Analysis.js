2e0e65ded4e1d78e9e664cabc56817f18280168e
Yashswi Singh <105166276+yashswi24@users.noreply.github.com>
2023-04-08 22:31:13 +0530

    Update Analysis.js

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -2,12 +2,34 @@ import React from 'react'
 
 const Analysis = () => {
   return (
-    <div
-    className='mt-10'
-    >
-      
-      <iframe src="https://cryptohunters.streamlit.app/" width="300" height="300px" ></iframe>
-    </div>
+    <section className="w-full min-h-[60vh] py-8  border border-gray-100 rounded flex flex-col items-center justify-center">
+      <p align="center">
+        The behavior of the cryptocurrency in order to predict future price movements and identify potential trading opportunities.<br></br>
+        This type of analysis can involve various techniques, such as technical analysis, fundamental analysis, and sentiment analysis.<br></br>
+        Technical analysis involves studying charts and indicators to identify patterns and trends in the price movements of Bitcoin
+      </p> 
+      <br></br>
+      <button
+        onClick={() => window.open("https://cryptohunters.streamlit.app/", "_blank")}
+        style={{
+          borderRadius: '5px',
+          width: '200px',
+          height: '50px',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          cursor: 'pointer',
+          transition: 'background-color 0.1s ease'
+        }}
+        // CSS hover styles
+        onMouseEnter={(e) => e.target.style.backgroundColor = '#3E8E41'}
+        onMouseLeave={(e) => e.target.style.backgroundColor = '#4CAF50'}
+      >
+        Link to Crypto Analysis website
+      </button>
+   </section>
   )
 }
 
